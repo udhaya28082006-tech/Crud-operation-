726aac29dc61c7a0e6e1ebb462bc2a8e22369d75
udhaya28082006-tech <udhaya28082006@gmail.com>
2026-01-02 14:21:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-sjdn
+const express = require("express");
+const app = express();
+const userRoutes = require("./routes/userRoutes");
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Routes
+app.use("/api/users", userRoutes);
+
+// Server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
